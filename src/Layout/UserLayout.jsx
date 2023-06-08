24f39b41dfd63759f135ed97339dd1da16793f0c
mahmoodyasir <yasirmamun16@gmail.com>
2023-06-08 23:50:15 +0600

    basic user nav 7 admin dashboard setup

--- src/Layout/UserLayout.jsx
+++ src/Layout/UserLayout.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Navbar from "../UserComponents/Navbar/Navbar";
+import {Outlet} from "react-router-dom";
 
 const UserLayout = () => {
     return (
-        <div>
-            <h1>USER</h1>
+        <div className="bg-amber-50 w-full h-screen">
+            <Navbar/>
+            <Outlet/>
+
         </div>
     );
 };
